79bdec557c79a4d2a4edf37b49d0c632644e78fd
Sandeep Raju <sandeepraju@practo.com>
2013-04-30 15:11:57 +0530

    added humans, robots & favicon routes

--- src/core/views.py
+++ src/core/views.py
@@ -5,9 +5,11 @@ import helpers as util
 
 # Standard python imports
 import json
+import os
 
 # Third party imports
-from flask import render_template
+from flask import render_template, send_from_directory
+
 
 # Application index
 @app.route("/")
@@ -19,3 +21,22 @@ def index():
 @app.errorhandler(404)
 def not_found(error=None):
     return render_template("404.html"), 404
+
+
+# Etc routes (favicon, robots.txt, humans.txt, etc.)
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'img/favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+@app.route('/robots.txt')
+def robots():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'robots.txt', mimetype='text/plain')
+
+
+@app.route('/humans.txt')
+def humans():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'humans.txt', mimetype='text/plain')
