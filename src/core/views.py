738e7f136dd113f75de08a80fa22b6f274bd1ec0
Sandeep Raju <sandeepraju@practo.com>
2013-04-30 14:08:16 +0530

    added 404 handler

--- src/core/views.py
+++ src/core/views.py
@@ -9,6 +9,13 @@ import json
 # Third party imports
 from flask import render_template
 
+# Application index
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+# Application error handlers
+@app.errorhandler(404)
+def not_found(error=None):
+    return render_template("404.html"), 404
